42e88ea7ea58b3e9aebef17d915522f680110d12
freelanceocero <199ocero@gmail.com>
2022-12-27 11:47:11 +0800

    add: custom post type

--- app/public/wp-content/themes/wordpress-sms/functions.php
+++ app/public/wp-content/themes/wordpress-sms/functions.php
@@ -31,6 +31,10 @@ function sms_posts_types()
         'public' => true,
         'labels' => array(
             'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
     ));
